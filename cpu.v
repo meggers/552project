4de103d95abc4c81223f436a3553803a09fa756f
Max Eggers <max.eggers@gmail.com>
2014-10-10 08:12:01 -0500

    misc changes

--- cpu.v
+++ cpu.v
@@ -3,11 +3,10 @@ module cpu(clk, rst_n, hlt, pc);
 //** DEFINE I/O INTERFACE **//
 input clk;        	// Global clock signal
 input rst_n;      	// Reset signal, active on low
-output hlt;       	// Halt signal
-output [15:0] pc; 	// Program counter
+output reg hlt;       	// Halt signal
+output reg [15:0] pc; 	// Program counter
 
 //** DEFINE GLOBAL VARS **//
-
 localparam Z = 0;	// Index for Zero flag
 localparam V = 1;	// Index for Overflow flag
 localparam N = 2;	// Index for Sign flag
@@ -35,28 +34,56 @@ reg [x:0] DATA_EX_MEM;
 reg [x:0] DATA_MEM_WB;
 
 //** DEFINE WIRES **//
+wire [15:0] instr;
 wire [8:0] ctrl_signals;
 
 //** DEFINE MODULES **//
-IM instr_mem(.clk(clk), addr, rd_en, instr);							// INSTRUCTION MEMORY
-DM  data_mem(.clk(clk), addr, re, we, wrt_data, rd_data);					// DATA MEMORY
-rf  reg_file(.clk(clk), p0_addr, p1_addr, p0, p1, re0, re1, dst_addr, dst, we, .hlt(hlt));	// REGISTER FILE
+IM instr_mem(.clk(clk), 		// INSTRUCTION MEMORY
+	     .addr(pc), 
+             rd_en,
+	     .instr(instr));	
+			
+DM  data_mem(.clk(clk),			// DATA MEMORY
+	     addr, re, we,
+	     wrt_data, rd_data);
 
-Control ctrl(instr, .ctrl_signals(ctrl_signals));						// CONTROL BLOCK
+rf  reg_file(.clk(clk), 		// REGISTER FILE
+	     .p0_addr(instr[7:4]), 
+	     .p1_addr(instr[3:0]), 
+	     p0, p1, re0, re1, 
+	     .dst_addr(11:8), dst, we, 
+	     .hlt(hlt));
 
+Control ctrl(.instr(instr[15:12]),	// CONTROL BLOCK
+	     .ctrl_signals(ctrl_signals));
+
+//** PROGRAM COUNTER **//
 always @(posedge clk or negedge rst_n) begin 
 	if (~rst_n) begin
-		pc <= 16'h0000;
+		pc <= 0;
 	end else begin
-		
+		pc <= pc + 4; // TODO: flesh this out
 	end
 end
 
 //** CONTROL PIPELINE **//
-always @(posedge clk) begin
-	CTRL_ID_EX <= ctrl_signals;
-	CTRL_EM_MEM <= CTRL_ID_EX[Branch:MemWrite];
-	CTRL_MEM_WB <= CTRL_EM_MEM[MemToReg:RegWrite];
+always @(posedge clk or negedge rst_n) begin
+	if (~rst_n) begin
+		CTRL_ID_EX  <= 0;
+		CTRL_EM_MEM <= 0;
+		CTRL_MEM_WB <= 0;
+	end else begin
+		CTRL_ID_EX  <= ctrl_signals;
+		CTRL_EM_MEM <= CTRL_ID_EX[Branch:MemWrite];
+		CTRL_MEM_WB <= CTRL_EM_MEM[MemToReg:RegWrite];
+	end
+end
+
+//** DATA PIPELINE **//
+always @(posedge clk or negedge rst_n) begin
+	if (~rst_n) begin
+	end else begin
+	end
 end
 
 endmodule
@@ -64,7 +91,7 @@ endmodule
 
 module Control(instr, ctrl_signals);
 
-input [5:0] instr;
+input [3:0] instr;
 output [8:0] ctrl_signals;
 
 endmodule
