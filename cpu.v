b374569030943a2506b805764eb48cdf9cf73835
Max Eggers <eggers.max@gmail.com>
2014-11-07 17:05:11 -0600

    fixed data rst

--- cpu.v
+++ cpu.v
@@ -11,14 +11,14 @@ localparam Z = 0;	// Index for Zero flag
 localparam V = 1;	// Index for Overflow flag
 localparam N = 2;	// Index for Sign flag
 
-localparam RegWrite = 0;
-localparam MemToReg = 1;
-localparam MemWrite = 2;
-localparam MemRead  = 3;
-localparam PCSrc    = 4;
-localparam ALUSrc   = 5;
-localparam ALUOpLSB = 6;
-localparam ALUOpMSB = 8;
+localparam RegWrite    = 0;
+localparam MemToReg    = 1;
+localparam MemWrite    = 2;
+localparam MemRead     = 3;
+localparam PCSrc       = 4;
+localparam ALUSrc      = 5;
+localparam ALUOpLSB    = 6;
+localparam ALUOpMSB    = 8;
 
 localparam IF_ID_PC    = 0;
 localparam IF_ID_INST  = 1;
@@ -38,7 +38,7 @@ localparam MEM_WB_RD   = 0;
 localparam MEM_WB_RSLT = 1;
 
 //** DEFINE REGISTERS **//
-reg [2:0] FLAG;		// Flag register
+reg [2:0] FLAG;
 
 reg [8:0] CTRL_ID_EX;
 reg [4:0] CTRL_EX_MEM;
@@ -56,40 +56,48 @@ reg [3:0] REG_MEM_WB;
 //** DEFINE WIRES **//
 wire [15:0] pc_incr, instr, read_1, 
 	    read_2, dm_read, op_2,
-	    result;
+	    result, write_data;
 
 wire [8:0] ctrl_signals;
 
 wire [2:0] flags;
 
+wire [1:0] read_signals;
+
 //** DEFINE MODULES **//
-IM instr_mem(.clk(clk), 		// INSTRUCTION MEMORY
+// INSTRUCTION MEMORY
+IM instr_mem(.clk(clk),
 	     .addr(pc), 
-             .rd_en(),// TODO
+             .rd_en(1'b1),
 	     .instr(instr));	
 			
-DM  data_mem(.clk(clk),			// DATA MEMORY
+// DATA MEMORY
+DM  data_mem(.clk(clk),
 	     .addr(DATA_EX_MEM[EX_MEM_RSLT]),
 	     .re(CTRL_EX_MEM[MemRead]), 
              .we(CTRL_EX_MEM[MemWrite]),
 	     .wrt_data(DATA_EX_MEM[EX_MEM_OP2]),
-	     .rd_data(dm_read));// TODO
+	     .rd_data(dm_read));
 
-rf  reg_file(.clk(clk), 		// REGISTER FILE
+// REGISTER FILE
+rf  reg_file(.clk(clk),
 	     .p0_addr(DATA_IF_ID[IF_ID_INST][11:8]), 
 	     .p1_addr(DATA_IF_ID[IF_ID_INST][7:4]), 
 	     .p0(read_1), 
 	     .p1(read_2),
-	     .re0(), .re1(),// TODO 
+	     .re0(read_signals[0]), 
+	     .re1(read_signals[1]), 
 	     .dst_addr(REG_MEM_WB), 
-	     .dst(),// TODO 
+	     .dst(write_data),
 	     .we(CTRL_MEM_WB[RegWrite]), 
 	     .hlt(hlt));
 
-Control ctrl(.instr(DATA_IF_ID[IF_ID_INST][15:12]),	// CONTROL BLOCK
-	     .ctrl_signals(ctrl_signals));
+// CONTROL BLOCK
+Control ctrl(.instr(DATA_IF_ID[IF_ID_INST][15:12]), 
+	     .ctrl_signals(ctrl_signals), 
+	     .read_signals(read_signals));
 
-
+// ALU
 module alu(.ALUop(CTRL_ID_EX[ALUOpMSB, ALUOpLSB]), 
 	   .src0(DATA_ID_EX[ID_EX_OP1]), 
 	   .src1(op_2), 
@@ -99,6 +107,7 @@ module alu(.ALUop(CTRL_ID_EX[ALUOpMSB, ALUOpLSB]),
 //** CONTINUOUS ASSIGNS **//
 assign pc_incr = pc + 4;
 assign op_2 = CTRL_ID_EX[ALUSrc] ? {12'h000, DATA_ID_EX[ID_EX_INST][3:0]} : DATA_ID_EX[ID_EX_OP1];
+assign write_data = CTRL_MEM_WB[MemToReg] ? DATA_MEM_WB[MEM_WB_RD] : DATA_MEM_WB[MEM_WB_RSLT];
 
 //** PROGRAM COUNTER **//
 always @(posedge clk or negedge rst_n) begin 
@@ -125,10 +134,25 @@ end
 //** DATA PIPELINE **//
 always @(posedge clk or negedge rst_n) begin
 	if (~rst_n) begin
-		DATA_IF_ID <= 0; // TODO: this doesn't work
-		DATA_ID_EX <= 0;
-		DATA_EX_MEM <= 0;
-		DATA_MEM_WB <= 0;
+		DATA_IF_ID[0] <= 16'h0000;
+		DATA_IF_ID[1] <= 16'h0000;
+
+		DATA_ID_EX[0] <= 16'h0000;
+		DATA_ID_EX[1] <= 16'h0000;
+		DATA_ID_EX[2] <= 16'h0000;
+		DATA_ID_EX[3] <= 16'h0000;
+
+		DATA_EX_MEM[0] <= 16'h0000;
+		DATA_EX_MEM[1] <= 16'h0000;
+		DATA_EX_MEM[2] <= 16'h0000;
+
+		DATA_MEM_WB[0] <= 16'h0000;
+		DATA_MEM_WB[1] <= 16'h0000;
+
+		REG_EX_MEM <= 4'b0000;
+		REG_MEM_WB <= 4'b0000;
+
+		FLAG <= 3'b000;
 	end else begin
 		DATA_IF_ID[IF_ID_PC] <= pc_incr;
 		DATA_IF_ID[IF_ID_INST] <= instr;
@@ -140,9 +164,9 @@ always @(posedge clk or negedge rst_n) begin
 
 		DATA_EX_MEM[EX_MEM_PC] <= DATA_ID_EX[ID_EX_PC];
 		DATA_EX_MEM[EX_MEM_RSLT] <= result;
-		FLAG <= flags;
 		DATA_EX_MEM[EX_MEM_OP2] <= DATA_ID_EX[ID_EX_OP2];
 		REG_EX_MEM <= DATA_ID_EX[ID_EX_INST][11:8];
+		FLAG <= flags;
 
 		DATA_MEM_WB[MEM_WB_RD] <= dm_read;
 		DATA_MEM_WB[MEM_WB_RSLT] <= DATA_EX_MEM[EX_MEM_RSLT];
@@ -151,26 +175,3 @@ always @(posedge clk or negedge rst_n) begin
 end
 
 module
-
-
-endmodule Control(instr, ctrl_signals);
-
-input [3:0] instr;
-output [8:0] ctrl_signals;
-
-localparam RegWrite = 0;
-localparam MemToReg = 1;
-localparam MemWrite = 2;
-localparam MemRead  = 3;
-localparam PCSrc    = 4;
-localparam ALUSrc   = 5;
-localparam RegDst   = 6;
-localparam ALUOpLSB = 7;
-localparam ALUUpMSB = 8;
-
-endmodule
-
-
-module aluControl(ALUOp, instr);
-
-endmodule
