ad0b42f426e9d088e3541edc0a8c74fa49008de0
Max Eggers <eggers.max@gmail.com>
2014-12-02 20:24:08 -0600

    instantiated supplied modules

--- mem_hierarchy.v
+++ mem_hierarchy.v
@@ -6,4 +6,46 @@ input clk, rst_n, re, we;
 output [15:0] instr, rd_data;
 output i_rdy, d_rdy;
 	
+cache Lcache(
+	clk,
+	rst_n,
+	addr,
+	wr_data,
+	wdirty,
+	we,
+	re,
+
+	rd_data,
+	tag_out,
+	hit,
+	dirty
+);
+
+cache Dcache(
+	clk,
+	rst_n,
+	addr,
+	wr_data,
+	wdirty,
+	we,
+	re,
+
+	rd_data,
+	tag_out,
+	hit,
+	dirty
+);
+
+unified_mem main_memory(
+	clk,
+	rst_n,
+	addr,
+	re,
+	we,
+	wdata,
+
+	rd_data,
+	rdy
+);
+
 endmodule
