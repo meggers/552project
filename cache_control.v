6c5a56e973883baa9e35e68cd31a56c07aa058d5
Max Eggers <eggers.max@gmail.com>
2014-12-06 17:54:43 -0600

    began defining control flow

--- cache_control.v
+++ cache_control.v
@@ -11,6 +11,45 @@ module cache_control(
 	i_data,	d_data,	m_data
 );
 
+//** STATES **//
+localparam CACHE_READ	= 2'b00;
+localparam MEM_READ	= 2'b01;
+localparam CACHE_MISS	= 2'b10;
+localparam WRITE_BACK	= 2'b11;
+
+//** REGISTERS **//
+reg [1:0] state, nextState; 
+
+//** CONTINUOUS ASSIGNS **//
 assign i_dirty_in = 1'b0;
+
+//** FSM STATES **//
+always $(posedge clk or negedge rst_n) begin
+	if (~rst_n) begin
+		state = CACHE_READ;
+	end else begin
+		state = nextState;
+	end
+end
+
+//** FSM LOGIC **//
+always @(*) begin
+	case (start)
+		CACHE_READ : begin
+		end
+		
+		MEM_READ : begin
+		end
+
+		CACHE_MISS : begin
+		end
+
+		WRITE_BACK : begin
+		end
+
+		default : begin
+		end
+	endcase
+end
 
 endmodule
